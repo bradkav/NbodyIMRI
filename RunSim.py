120f3a4e7f117663315d25d4d1a2a8c3eea88a1d
Bradley Kavanagh <kavanaghb@login1.ifca.es>
2022-06-06 10:45:36 +0200

    Added command line option for RunSim.py

--- RunSim.py
+++ RunSim.py
@@ -1,6 +1,7 @@
 import units as u
 import tools
-import DIY
+import particles
+import simulator
 
 import DistributionFunctions as DF
 
@@ -10,7 +11,7 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-a_i', '--a_i', type=float, default=3e-8)
 args = parser.parse_args()
 
-M_1 = 1000.0*u.Msun
+M_1 = 300.0*u.Msun
 M_2 = 1*u.Msun
 
 a_i = args.a_i*u.pc
@@ -21,23 +22,23 @@ rho_6 = tools.calc_rho_6(rho_sp, M_1, gamma=7/3)
 
 N_DM = 2**17
 
-r_t = 1.5e-7*u.pc
+r_t = 20*a_i
 alpha = 2
 
 print("> Initialising system...")
 
-p_dressed_binary = DIY.particles_in_binary(M_1, M_2, a_i, e_i, N_DM = N_DM, r_max = 1e-3*u.pc, rho_6=rho_6, r_t=r_t, alpha=alpha)
+p_dressed_binary = particles.particles_in_binary(M_1, M_2, a_i, e_i, N_DM = N_DM, r_max = 1e-3*u.pc, rho_6=rho_6, r_t=r_t, alpha=alpha)
 
-sim = DIY.simulator(p_dressed_binary, soft_method="uniform")
+sim = simulator.simulator(p_dressed_binary, soft_method="uniform")
 
-eps = 5e-11*u.pc
+eps = 3e-11*u.pc
 sim.r_soft_sq = eps**2
 
 
 T_orb = sim.p.T_orb()
-N_step_per_orb = 10000
+N_step_per_orb = 20000
 
-N_orb = 5
+N_orb = 10
 t_end = T_orb*N_orb
 dt    = T_orb/N_step_per_orb
 
